9180d2247f86aa87f051d9d58bf87c36587a7d54
LiJiaRong-jpg <83932363+LiJiaRong-jpg@users.noreply.github.com>
2021-05-19 16:38:43 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<style>
+	/*每个页面公共css */
+	@font-face {
+	  font-family: 'iconfont';  /* Project id 2537631 */
+	  src: url('//at.alicdn.com/t/font_2537631_k68vaf6jjtn.woff2?t=1621385043127') format('woff2'),
+	       url('//at.alicdn.com/t/font_2537631_k68vaf6jjtn.woff?t=1621385043127') format('woff'),
+	       url('//at.alicdn.com/t/font_2537631_k68vaf6jjtn.ttf?t=1621385043127') format('truetype');
+	}
+
+	.iconfont {
+		width: 7vh;
+		font-family: "iconfont" !important;
+		font-size: 25px;
+		color: black;
+		font-style: normal;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+</style>
